f7309449291028bda2d2d6ad37e6462f30a70cb1
Tim Kelly <timpkelly1@gmail.com>
2020-02-09 09:51:32 +0000

    Stuck on DOM manipulation, I am going to learn more on The Odin Project and come back when I'm ready to finish

--- project.js
+++ project.js
@@ -1,71 +1,103 @@
 /// Creating the variables
-const choices = ["rock", "paper", "scissors"];
-let playerSelection = "paper";
+const computerSelect = ["rock", "paper", "scissors"];
+let playerSelect;
 let computerSelection;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let topScore = 0;
+let roundCount = 0;
 
-/// Random choice for the computer (scissor, paper or rock)
+/// Function to generate a random choice for the computer (scissors, paper or rock)
 function computerPlay() {
-  computerSelection = choices[Math.floor(Math.random() * choices.length)];
+  computerSelection = computerSelect[Math.floor(Math.random() * computerSelect.length)];
   return computerSelection;
 }
 console.log(computerPlay())
 
-/// Convert playerSelection to lowercase (requested by The Odin Project)
-function playerPlay(playerSelection) {
-    selectionLower = playerSelection.toLowerCase();
-    if (selectionLower = "rock") {
-        playerSelection = "rock";
-    }
-    else if (selectionLower = "paper") {
-        playerSelection = "paper"
-    }
-    else {
-        playerSelection = "scissors"
-    }
-    return playerSelection;
+/// Function for player to choose scissors, paper or rock
+
+
+
+/// Function to update the 'round'
+function updateRound() {
+        let roundHeading = document.querySelector("#round-count-top");
+        
+        roundCount++;
+        roundHeading.innerHTML = "Round: " + roundCount;
+      }
+
+/// Function to update the 'top score'
+function updateTopScore() {
+        if (playerScore > computerScore) {
+                topScore = playerScore
+        }
+        else {
+                topScore = computerScore
+        }
 }
+     
 
-console.log(playerSelection)
-
-/// Play one round of scissor, paper, rock
+///  Function to play one round of scissor, paper, rock
 function playRound(playerSelection, computerSelection) {
-    /// Outcomes for player chooses rock
-    if(playerSelection == "rock" && computerSelection == "rock") {
-            alert("You draw! You both picked rock.");
-            }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-            alert("You win! Rock beats scissors.");
-            }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
-            alert("You lose! Paper beats rock.");
+        /// Outcomes if player chooses rock
+        if(playerSelection == "rock" && computerSelection == "rock") {
+                alert("You draw! You both picked rock.");
+                updateRound();
+                }
+        else if (playerSelection == "rock" && computerSelection == "scissors") {
+                alert("You win! Rock beats scissors.");
+                ++playerScore;
+                updateTopScore();
+                updateRound();
+                }
+        else if (playerSelection == "rock" && computerSelection == "paper") {
+                alert("You lose! Paper beats rock.");
+                ++computerScore;
+                updateTopScore();
+                updateRound();
+        }
+    
+        /// Outcomes if player chooses scissors
+        else if (playerSelection == "scissors" && computerSelection == "scissors") {
+                alert("You draw! You both picked scissors.");
+                updateRound();
+                }
+        else if (playerSelection == "scissors" && computerSelection == "rock") {
+                alert("You lose! Rock beats scissors");
+                ++computerScore;
+                updateTopScore();
+                updateRound();
+        }
+        else if (playerSelection == "scissors" && computerSelection == "paper") {
+                alert("You win! Scissors beats paper.");
+                ++playerScore;
+                updateTopScore();
+                updateRound();
+        }
+    
+        /// Outcomes if player chooses paper
+        else if (playerSelection == "paper" && computerSelection == "paper") {
+                alert("You draw! You both picked paper.");
+                updateRound();
+                }
+        else if (playerSelection == "paper" && computerSelection == "rock") {
+                alert("You win! Paper beats rock");
+                ++playerScore;
+                updateTopScore();
+                updateRound();
+                }
+        else if (playerSelection == "paper" && computerSelection == "scissors") {
+                alert("You lose! Scissors beats paper");
+                ++computerScore;
+                updateTopScore();
+                updateRound();
+        }
+    
+        /// Outcome if player chooses a different result
+        else {
+                alert("Unexpected result");
+        }
     }
 
-    /// Outcomes for player chooses scissors
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-            alert("You draw! You both picked scissors.");
-            }
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-            alert("You lose! Rock beats scissors")
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-            alert("You win! Scissors beats paper.");
-    }
-
-    /// Outcomes for player chooses paper
-    else if (playerSelection == "paper" && computerSelection == "paper") {
-            alert("You draw! You both picked paper.");
-            }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-            alert("You win! Paper beats rock");
-            }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
-            alert("You lose! Scissors beats paper")
-    }
-
-    /// Outcome if player chooses a different result
-    else {
-            alert("Unexpected result");
-    }
-}
-
-console.log(playRound(playerSelection, computerSelection))
+    /// Function to play 5 rounds
